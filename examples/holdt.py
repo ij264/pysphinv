c0fbb70bbd70672699a847f85bddc6faf8b2fa90
ij264 <ij264@cam.ac.uk>
2025-09-04 13:26:32 +0100

    bit broken still!

--- examples/holdt.py
+++ examples/holdt.py
@@ -1,16 +1,11 @@
 import numpy as np
 
-from pysphinv.spherical_harmonic_inversion import SphericalHarmonicInversion, SphericalHarmonicsUtils, InversionData
+from spherical_harmonic_inversion import SphericalHarmonicInversion, InversionData, SphericalHarmonicsUtils
 
 data = InversionData(data_file='/space/ij264/earth-tunya/data/2024/global_holdt.xyz')
-inversion = SphericalHarmonicInversion(data)
+inversion = SphericalHarmonicInversion(data, l_max=40)
+solution = inversion.solve()
 
-lambda_norms = np.logspace(1.5, 3.5, 5)
-lambda_grads = np.logspace(-1, 1, 5)
-
-# inversion.solve(lambda_norm=400, lambda_grad=1. )
-# inversion.plot_grid(fname='/space/ij264/earth-tunya/pysphinv/tests/holdt.png')
-# print(inversion.misfit)
-inversion.calculate_spectrum_family(lambda_norms, lambda_grads)
-inversion.plot_power_spectrum(fname='/space/ij264/earth-tunya/pysphinv/tests/holdt_ps_new.png')
+coeffs = SphericalHarmonicsUtils.clm_to_vector(solution)
+np.savetxt('holdt_coeffs.txt', coeffs)
 
